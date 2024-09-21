64f15c2cfb5f06f3c4abefe8e3af8b061090fa4c
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2024-09-21 11:07:24 +0300

    added custom prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  useTabs: false,
+  printWidth: 80,
+  semi: true,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  objectCurlySpacing: ['error', 'never'],
+  objectCurlySpacing: ['error', 'never'],
+  quotes: ['error', 'double'],
+  noTabs: 'error',
+  indent: ['error', 2],
+};
