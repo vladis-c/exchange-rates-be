26c5992777b5237778848482c20b25aca96f0772
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2024-09-26 20:54:48 +0300

    amount query param in conversions endopoint is string

--- src/conversions/conversions.controller.ts
+++ src/conversions/conversions.controller.ts
@@ -11,19 +11,21 @@ export class ConversionsController {
   async getCurrencies(
     @Param('base_currency') base_currency: string,
     @Param('quote_currency') quote_currency: string,
-    @Query('amount') amount: number,
+    @Query('amount') amount: string,
   ): Promise<ConversionRate> {
     const exchangeRate = await getConversion(
       this.exchangeRatesService,
       base_currency,
       quote_currency,
     );
+
     const conversionRate: ConversionRate = {
       ...exchangeRate,
       base_amount: +amount,
-      quote_amount: exchangeRate.quote * amount,
+      quote_amount: exchangeRate.quote * +amount,
       opposite_quote: +(1 / exchangeRate.quote).toFixed(6),
     };
-    return new Promise<ConversionRate>(res => res(conversionRate));
+    console.log('conversionRate', conversionRate);
+    return conversionRate;
   }
 }
